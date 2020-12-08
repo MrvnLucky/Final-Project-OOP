78340c83943d80c3e1f8db1d9711521778ffafc7
Matthew Felix <matthewfelix88@gmail.com>
2020-12-09 00:51:20 +0700

    Added Instruction Screen

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Toolkit;
@@ -8,8 +9,10 @@ import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import javax.swing.Timer;
+
 
 @SuppressWarnings("serial")
 public class Board extends JPanel implements ActionListener {
@@ -227,21 +230,97 @@ public class Board extends JPanel implements ActionListener {
     }
   }
 
+  // void endGame(Graphics g) {
+
+  //   // Create a message telling the player the game is over
+  //   String message = "Game over";
+
+  //   // Create a new font instance
+  //   Font font = new Font("Times New Roman", Font.BOLD, 30);
+  //   FontMetrics metrics = getFontMetrics(font);
+
+  //   // Set the color of the text to red, and set the font
+  //   g.setColor(Color.red);
+  //   g.setFont(font);
+
+  //   // Draw the message to the board
+  //   g.drawString(message, (BOARDWIDTH - metrics.stringWidth(message)) / 2, BOARDHEIGHT / 2);
+
+  //   System.out.println("Game Ended");
+
+  // }
+
   void endGame(Graphics g) {
 
     // Create a message telling the player the game is over
-    String message = "Game over";
+    Rectangle instrBox = new Rectangle(Board.BOARDWIDTH / 6 - 20 ,Board.BOARDHEIGHT/5 + 50,700,400);
+    String message = "S N E C C";
+    String message1= "Instruction";
+    String message2= "To play the game, you basically press";
+    String message3= "Directional arrow";
+    String message4= "in your keyboard to change";
+    String message5= "Your S n e c c Direction, That is all";
+    String arrow ="\u2190     \u2192      \u2191      \u2193";
+    String direction="<Left>      <RIGHT>         <UP>       <DOWN>";
+    String message6= "Press SPACEBAR to Start";
+
 
     // Create a new font instance
-    Font font = new Font("Times New Roman", Font.BOLD, 14);
-    FontMetrics metrics = getFontMetrics(font);
+    Font font = new Font("Courier New" ,Font.BOLD, 50);
+    Font font1 = new Font("Arial" ,Font.BOLD, 30);
+    Font font2 = new Font("Courier New" ,Font.BOLD, 20);
+    Font font3 = new Font("Courier New" ,Font.BOLD, 20);
+    Font font4 = new Font("Courier New" ,Font.BOLD, 20);
+    Font font5 = new Font("Courier New" ,Font.BOLD, 20);
+    Font taptostart = new Font("Arial Narrow" ,Font.ITALIC, 50);
+    Font arrowFont = new Font("Sans Serif",Font.BOLD,50);
+    Font directionFont = new Font("Courrier New",Font.BOLD,20);
 
+    // FontMetrics metrics = getFontMetrics(font);
+    // FontMetrics metrics1 = getFontMetrics(font1);
+    // FontMetrics metrics2 = getFontMetrics(font2);
+    // FontMetrics metrics3 = getFontMetrics(font3);
+    // FontMetrics metrics4 = getFontMetrics(font4);
+    
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.draw(instrBox);
+    g2d.setColor(Color.green);
+    // g2d.SetRectangle(instrBox);
     // Set the color of the text to red, and set the font
-    g.setColor(Color.red);
+
+    g.setColor(Color.GREEN);
     g.setFont(font);
+    g.setFont(font1);
+    g.setFont(font2);
+    g.setFont(font3);
+    g.setFont(font4);
+    
 
     // Draw the message to the board
-    g.drawString(message, (BOARDWIDTH - metrics.stringWidth(message)) / 2, BOARDHEIGHT / 2);
+    g.setColor(Color.GREEN);
+    g.setFont(font);
+    g.drawString(message, 350, 100);
+    g.setColor(Color.GRAY);
+    g.setFont(font1);
+    g.drawString(message1, 410, 200);
+    g.setColor(Color.LIGHT_GRAY);
+    g.setFont(font2);
+    g.drawString(message2, 250, 300);
+    g.setFont(font3);
+    g.drawString(message3, 250, 330);
+    g.setFont(font4);
+    g.drawString(message4, 250, 360);
+    g.setFont(font5);
+    g.drawString(message5, 250,390);
+
+    g.setFont(arrowFont);
+    g.drawString(arrow, 300, 450);
+    g.setFont(directionFont);
+    g.drawString(direction, 300,480);
+
+    g.setColor(Color.RED);
+    g.setFont(taptostart);
+    g.drawString(message6,250,600);
 
     System.out.println("Game Ended");
 
